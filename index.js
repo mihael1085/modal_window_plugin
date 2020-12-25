ec5fd171bea0785c5fd840c11c101bf67e0dbe22
mihael1085 <mihael1085@gmail.com>
2020-12-25 22:32:56 +0300

    End project course

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const modal = $.modal()
+const options = {
+    title: 'My first modal window',
+    isClosable: true,
+    content: `  <p>Lorem ipsum dolor sit amet consectetur</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            `,
+    width: '',
+    footerButtons: [
+    {text: 'ok', type: 'primary', handler() {
+    	console.log('primary button clicked')
+    }},
+    {text: 'Cancel', type: 'danger', handler() {
+    	console.log('danger button clicked')
+    }},
+    ]
+}
+
+const modal = $.modal(options)
--- plugin/modal.js
+++ plugin/modal.js
@@ -1,20 +1,39 @@
+
+
+Element.prototype.appendAfter = function(){}
+
+_createFooter = function(buttons) {
+
+        if(buttons.length == 0)
+            return document.createElement('div')
+        
+        const $footer = document.createElement('div')
+        $footer.classList.add('modal-footer')
+
+        buttons.forEach((button) => {
+            const $btn = document.createElement('button')
+            $btn.classList.add(button.type)
+            $btn.textContent = button.text
+            $btn.addEventListener('click', button.handler)
+            $footer.append($btn)
+        })
+
+        return $footer
+    }
+
 _createModal = function(options) {
+    const DEFAULT_WIDTH = '600px'
     const modal = document.createElement('div')
     modal.classList.add('vmodal')
     modal.insertAdjacentHTML('afterbegin', `
-        <div class="modal-overlay">
-            <div class="modal-window">
+        <div class="modal-overlay" data-close="true">
+            <div class="modal-window" style="width:${options.width || DEFAULT_WIDTH}">
                 <div class="modal-header">
-                    <span class="modal-title">This is my modal window</span>
-                    <span class="modal-close">&times;</span>
+                    <span class="modal-title"> ${options.title || 'modal window'}</span>
+                    ${options.isClosable ? '<span class="modal-close" data-close="true">&times;</span>' : ''}
                 </div>
-                <div class="modal-body">
-                    <p>Lorem ipsum dolor sit amet consectetur</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                </div>
-                <div class="modal-footer">
-                    <button>Ok</button>
-                    <button>Cancel</button>
+                <div class="modal-body" data-content>
+                    ${options.content || ''}
                 </div>
             </div>
         </div>
@@ -24,15 +43,41 @@ _createModal = function(options) {
     return modal
 }
 
+
 $.modal = function(options) {
     const $modal = _createModal(options)
+    const ANIMATION_DELAY = 200
+    let destroyed = false
 
-    return {
+    const listener = (el) => { if (el.toElement.dataset.close)
+                                    methods.close()
+                                }
+
+    const $footer = _createFooter(options.footerButtons)
+    document.querySelector('.modal-window').appendChild($footer)
+
+    const methods = {
         open() {
+            if(destroyed) console.log('modal is destroyed')
             $modal.classList.add('open')
         },
         close() {
             $modal.classList.remove('open')
-    } 
-}
+            $modal.classList.add('hide')
+            setTimeout(() => $modal.classList.remove('hide')
+                , ANIMATION_DELAY)
+        },
+        destroy() {
+            $modal.removeEventListener('click', listener)
+            $modal.parentNode.removeChild($modal)
+            destroyed = true
+        },
+        setContent(html) {
+            $modal.querySelector('[data-content]').innerHTML = html
+        }
+    }
+
+    $modal.addEventListener('click', listener)
+
+    return methods
 }
